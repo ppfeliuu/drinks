fdfaa7ca9c9eaf32e12075e13d4be664eace0e6c
ppfeliuu <ppfeliuu@gmail.com>
2020-03-13 16:45:20 +0000

    New changes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,67 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
 
+
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+  const top = 50 ;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
 const Recipe = ({ recipe }) => {
-  const { setIdRecipe } = useContext(ModalContext);
+
+  const [ modalStyle ] = useState(getModalStyle)
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const { inforecipe, setIdRecipe, setRecipe } = useContext(ModalContext);
+
+  console.log(inforecipe)
+
+  const showIngredients = inforecipe => {
+    let ing = [];
+
+    for (let i = 1; i < 16; i++) {
+      if ( inforecipe[`strIngredient${i}`]) {
+        ing.push(<li>
+          {inforecipe[`strIngredient${i}`]} {' '}
+          {inforecipe[`strMeasure${i}`]}
+        </li>)
+      }
+    }
+
+    return ing;
+  }
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
@@ -20,10 +79,29 @@ const Recipe = ({ recipe }) => {
             className="btn btn-block btn-primary"
             onClick={() => {
               setIdRecipe(recipe.idDrink);
+              handleOpen();
             }}
           >
             View
           </button>
+
+          <Modal open={open} onClose={() => {setIdRecipe(null); setRecipe({}); handleClose()}}>
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{inforecipe.strDrink}</h2>
+              <h3 className="mt-4">Instructions</h3>
+              <p>
+                {inforecipe.strInstructions}
+              </p>
+
+              <img className="img-fluid my-4" src={inforecipe.strDrinkThumb} alt={inforecipe.strDrink} />
+
+              <h3>Ingredients and Quantities</h3>
+
+              <ul>
+                { showIngredients(inforecipe)}
+              </ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -5,9 +5,26 @@ export const ModalContext = createContext();
 
 const ModalProvider = props => {
   const [idrecipe, setIdRecipe] = useState(null);
+  const [inforecipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      if (!idrecipe) return;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idrecipe}`;
+
+      const res = await axios.get(url);
+
+      setRecipe(res.data.drinks[0])
+            
+    }
+
+    getRecipe();
+
+  },[idrecipe])
 
   return (
-    <ModalContext.Provider value={{ setIdRecipe }}>
+    <ModalContext.Provider value={{ inforecipe, setIdRecipe, setRecipe }}>
       {props.children}
     </ModalContext.Provider>
   );
